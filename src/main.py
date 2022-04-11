8c0f2b2ef84314a15b3a65075eeae79ee74d2edd
Paul Machuel <paul.machuel@esme.fr>
2022-04-11 21:32:44 +0200

    webscrapping google images

--- src/main.py
+++ src/main.py
@@ -1,39 +1,53 @@
-import os
-from tkinter.tix import IMAGE 
+from tkinter import image_names
+import requests
+import urllib.request
+from PIL import Image
+from bs4 import BeautifulSoup
 
-# importing google_images_download module
-from google_images_download import google_images_download 
+URL = "https://www.google.com/search?q=alma+bag+louis+vuitton&tbm=isch&ved=2ahUKEwi8wtqU0oz3AhUI3RoKHRwTBLAQ2-cCegQIABAA&oq=alma+bag&gs_lcp=CgNpbWcQARgBMgUIABCABDIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjoKCCMQ7wMQ6gIQJzoHCCMQ7wMQJzoECAAQQzoICAAQsQMQgwE6CAgAEIAEELEDOgQIABADOgsIABCABBCxAxCDAVDzBViZF2DAJmgBcAB4AIABN4gBrQOSAQE5mAEAoAEBqgELZ3dzLXdpei1pbWewAQrAAQE&sclient=img&ei=tXNUYvyTKIi6a5ymkIAL"
 
-IMAGE_FOLDER = "images"
+page = requests.get(URL)
 
-# creating object
-client = google_images_download.googleimagesdownload() 
-  
-search_query = "Louis Vuitton Alma bag"
-      
-def downloadimages(query):
-    print(query)
-    # keywords is the search query
-    # format is the image file format
-    # limit is the number of images to be downloaded
-    # print urs is to print the image file url
-    # size is the image size which can
-    # be specified manually ("large, medium, icon")
-    # aspect ratio denotes the height width ratio
-    # of images to download. ("tall, square, wide, panoramic")
-    arguments = {"keywords": query,
-                 "format": "jpg",
-                 "limit":10,
-                 "print_urls":True,
-                 "size": "medium",
-                 "output_directory": IMAGE_FOLDER
-                 }
-  
-    client.download(arguments)
 
-if not os.path.exists(IMAGE_FOLDER):
-    os.makedirs(IMAGE_FOLDER)   
-  
-# Driver Code
-downloadimages(search_query) 
- 
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
+image_tags = soup.find_all('img')
+links = []
+for image_tag in image_tags:
+    links.append(image_tag['src'])
+
+links = links[1:]
+print(links[0])
+urllib.request.urlretrieve(links[0], "images/alma_bag.jpg")
+
+for link in links:
+    image_name = link.split("tbn:")[1]
+    urllib.request.urlretrieve(link, "images/{}.jpg".format(image_name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
