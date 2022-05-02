642d8e104434c60e7febdec4684245aff2333f21
Yinshangyi <jl.dejaeger@gmail.com>
2022-05-02 20:55:03 +0200

    Get HTML page with Selenium and exception handling for the src field in the img tags

--- src/image_fetcher.py
+++ src/image_fetcher.py
@@ -1,11 +1,15 @@
-import requests
 import urllib.request
 import os
-import ssl
-from requests.utils import quote
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import random
+import ssl
 
 class ImageFetcher:
+
+    ssl._create_default_https_context = ssl._create_unverified_context
 
     def __init__(self):
         self.url = None
@@ -17,22 +21,35 @@ class ImageFetcher:
         self.url = url
         print(url)
 
+    def get_html(self):
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get(self.url)
+        html = driver.page_source
+        return html
+
     def get_images_links(self):
-        page = requests.get(self.url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        image_tags = soup.find_all('img', class_ = "yWs4tf")
-
+        html = self.get_html()
+        print("Get HTML from url")
+        soup = BeautifulSoup(html, 'html.parser')
+        image_tags = soup.find_all('img')
+        
         for image_tag in image_tags:
-            print(image_tag["src"])
-            self.img_urls.append(image_tag["src"])
-
+            try:
+                self.img_urls.append(image_tag["src"])
+            except KeyError:
+                try:
+                    self.img_urls.append(image_tag["data-src"])
+                except KeyError:
+                    print("Neither src or data-src was found in the tag")
+        print("{} images urls has been found".format(len(self.img_urls)))
 
     def download_images(self, output_folder):
         for url in self.img_urls:
             if not os.path.exists(output_folder):
                 os.makedirs(output_folder)
-            image_name = url.split("tbn:")[1]
-            urllib.request.urlretrieve(url, "images/{}.jpg".format(image_name))
+            #image_name = url.split("tbn:")[1]
+            image_name = str(random.randint(1, 1000))
+            urllib.request.urlretrieve(url, "images/img_{}.jpg".format(image_name))
 
 
 
